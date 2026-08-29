e32295fac86c8692ccfb527637bfc6440de57e50
kawalzzz <kawalpreetk396@gmail.com>
2026-08-29 14:04:54 +0530

    compleated project

---
+++ script.js
@@ -0,0 +1,51 @@
+let bagItems;
+init();
+
+function init() {
+  let bagItemStr = localStorage.getItem('bagItems');
+  bagItems = bagItemStr ? JSON.parse(bagItemStr) : [];
+  displayItems();
+  displayBagIcon();
+}
+
+function addToBag(itemId) {
+  bagItems.push(itemId);
+  localStorage.setItem('bagItems', JSON.stringify(bagItems));
+  displayBagIcon();
+}
+
+function displayBagIcon() {
+  let bagItemCountElement = document.querySelector('.bag-item-count');
+  if (bagItems.length > 0) {
+    bagItemCountElement.style.visibility = 'visible';
+    bagItemCountElement.innerText = bagItems.length;
+  } else {
+    bagItemCountElement.style.visibility = 'hidden';
+  }
+}
+
+function displayItems() {
+  let itemsContainerElement = document.querySelector('.items-container');
+  if (!itemsContainerElement) {
+    return;
+  }
+
+  let innerHTML = '';
+  items.forEach(item => {
+    innerHTML += `
+    <div class="item-container">
+      <img class="item-image" src="${item.image}" alt="item image" />
+      <div class="rating">${item.rating.stars}⭐ | ${item.rating.count} </div>
+      <div class="company-name">${item.company}</div>
+      <div class="item-name">${item.item_name}</div>
+      <div class="price">
+        <span class="current-price">Rs ${item.current_price}</span>
+        <span class="original-price">Rs ${item.original_price}</span>
+        <span class="discount">(${item.discount_percentage}% Off)</span>
+      </div>
+      <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+    </div>
+    `;
+  });
+  itemsContainerElement.innerHTML = innerHTML;
+}
